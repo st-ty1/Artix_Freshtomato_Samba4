3397770f56ba4ed220d18912846e456bf8b16f2f
st-ty1 <58690468+st-ty1@users.noreply.github.com>
2020-06-07 19:52:12 +0200

    move compile_et and asn1_compile to test folder, delete Artix patches and update README.md

--- README.md
+++ README.md
@@ -4,26 +4,19 @@ How to build Samba4 for FreshTomato-mips
 
 Steps needed:
 
-1.) Download source of samba-4.9.18 (https://download.samba.org/pub/samba/samba-4.9.18.tar.gz) and extract it into a first folder in your home directory.
+1.) Download source of samba-4.9.18 (https://download.samba.org/pub/samba/samba-4.9.18.tar.gz) and extract it into a first subfolder of your home directory.
 
-2.) Download this repo into a second folder in your home directory. Depending on you want compile for MIPSEL- or for ARM-router, also copy Makefile_RT-AC.patch or Makefile_arm.patch, common.mak.patch, genconfig.sh.patch, configure.ac_tor.patch and mksquashfs.c.patch from my github repo Artix_FreshTomato (https://github.com/st-ty1/Artix_FreshTomato) into this folder. 
+2.) Download this repo into a second subfolder of your home directory. 
 
-3.) Download source of libtirpc_1.1.4 (https://sourceforge.net/projects/libtirpc/files/libtirpc/1.1.4/libtirpc-1.1.4.tar.bz2/download) and extract it into a third folder in your home directory.
+3.) Download Artix_Freshtomato repo (https://github.com/st-ty1/Artix_FreshTomato) into third subfolder of your home directory. 
 
-4.) Start shell script build_ft-mips_samba.sh in folder of this repo.
+4.) Download source of libtirpc_1.1.4 (https://sourceforge.net/projects/libtirpc/files/libtirpc/1.1.4/libtirpc-1.1.4.tar.bz2/download) and extract it into a forth subfolder of your home directory.(samba4 depends on libtirpc) 
 
+5.) Change the sym-link to python in /usr/bin to from python3 to python2 (Artix/Arch Linux OS).
 
-Rem: To build Freshtomato(mips) with samba4 in Artix,
-  - the sym-link to python in /usr/bin has to be changed from python3 to python2. 
-  - release/src/router/Makefile need modifications:
-      o some Artix-related patches are needed as documented in https://github.com/st-ty1/Artix_FreshTomato/blob/master/modification_FT_sources.txt.
-      o as samba4 depends on libtirpc targets for libtirpc have to be implemented in release/src/router/Makefile. 
-      o two of "samba3" passages in release/src/router/Makefile has to be replaced by "samba4" passages.
-  - release/src/router/nettle/desdata.stamp has to be removed.
-  - Makefile in release/src/router/samba has to be replaced.
-    Remember that install-target of this Makefile is not optimized yet, i.e. also libfoo.pl has to be adapted because of newer and more libraries to be striped compared to samba-3.6.
-  
-   All these steps above are incorporated in shell script build_ft-mips_samba.sh.
+6.) Start shell script build_ft-mips_samba.sh of local copy of this repo. (This script-file patches FT-sources for use on Artix/Arch Linux, inserts samba4 sources, the samba4-own Makefile, the answer-file (need for cross-compiling samba4 with waf) and libtirpc sources into FT-sources. It modifies also the Makefile of FT by inserting new targets for libtirpc and replaces two positions "samba3" by "samba4".)
+ 
+Remember that install-target of this Makefile is not optimized yet, i.e. also libfoo.pl has to be adapted because of newer and more libraries to be striped compared to samba-3.6.
 
 BR
 st-ty1
--- build_ft-mips_samba.sh
+++ build_ft-mips_samba.sh
@@ -1,60 +1,57 @@
 #! /bin/sh
 
-## toolchain paths
-export PATH="/home/stephan/freshtomato-mips/tools/brcm/hndtools-mipsel-linux/bin:$PATH"
-export PATH="/home/stephan/freshtomato-mips/tools/brcm/hndtools-mipsel-uclibc/bin:$PATH"
-export PATH="/usr/lib/ccache/bin/:$PATH"
-
 ## path to Artix-patch folder for building FT
-FT_MIPS_ARTIX_PATCHDIR=$HOME/Dokumente/freshtomato-mips
+FT_PATCHES_DIR=$HOME/Documents/freshtomato-mips
 
 ## path to the FT-repo folder
-FT_MIPS_REPODIR=$HOME/freshtomato-mips
+FT_REPO_DIR=$HOME/freshtomato-mips
 
 ## path to your local repo of FT-samba4
-FT_SAMBA_DIR=$HOME/Dokumente/freshtomato-samba4
+FT_SAMBA_DIR=$HOME/Documents/freshtomato-samba4
 
 ## path to extracted libtirpc-sources
-LIBTIRPC_DIR=$HOME/Dokumente/freshtomato-samba4/libtirpc_1.1.4
+LIBTIRPC_DIR=$FT_SAMBA_DIR/libtirpc_1.1.4
 
-cd $FT_MIPS_REPODIR 
+## toolchain paths
+export PATH="$FT_REPO_DIR/tools/brcm/hndtools-mipsel-linux/bin:$PATH"
+export PATH="$FT_REPO_DIR/tools/brcm/hndtools-mipsel-uclibc/bin:$PATH"
+
+cd $FT_REPO_DIR 
 git clean -dxf 
 git reset --hard
 
 ## for RT-images:
 #git checkout mips-master
-## Achtung : libnfs-patch noch nicht enthalten; unklar, ob sinnvoll
-#patch -i $FT_SAMBA_DIR/Makefile_master.patch $FT_MIPS_REPODIR/release/src/router/Makefile
-
 ## for RT-N- and RT-AC-images:
 git checkout mips-RT-AC
-patch -i $FT_SAMBA_DIR/Makefile_RT-AC.patch $FT_MIPS_REPODIR/release/src/router/Makefile
-
 
 ## Arch-Linux-patches for all builds
-patch -i $FT_MIPS_ARTIX_PATCHDIR/common.mak.patch $FT_MIPS_REPODIR/release/src/router/common.mak
-patch -i $FT_MIPS_ARTIX_PATCHDIR/Makefile.linux.patch $FT_MIPS_REPODIR/release/src/router/miniupnpd/Makefile.linux
-patch -i $FT_MIPS_ARTIX_PATCHDIR/genconfig.sh.patch $FT_MIPS_REPODIR/release/src/router/miniupnpd/genconfig.sh
-rm -f $FT_MIPS_REPODIR/release/src/router/nettle/desdata.stamp
+ppatch -i $FT_PATCHES_DIR/common.mak.patch $FT_REPO_DIR/release/src/router/common.mak
+### error: "uuid/uuid.h: No such file or directory"
+patch -i $FT_PATCHES_DIR/genconfig.sh.patch $FT_REPO_DIR/release/src/router/miniupnpd/genconfig.sh
+patch -i $FT_PATCHES_DIR/Makefile.patch $FT_REPO_DIR/release/src/router/Makefile
+patch -i $FT_PATCHES_DIR/configure.ac_tor.patch $FT_REPO_DIR/release/src/router/tor/configure.ac
+
+patch -i $FT_PATCHES_DIR/Makefile_config.patch $FT_REPO_DIR/release/src/router/config/Makefile
+patch -i $FT_PATCHES_DIR/Makefile_squashfs_RT-AC.patch $FT_REPO_DIR/release/src-rt-6.x/linux/linux-2.6/scripts/squashfs/Makefile
+
+rm -f $FT_REPO_DIR/release/src/router/nettle/desdata.stamp
 
 ## patches for samba4
-mkdir $FT_MIPS_REPODIR/release/src/router/samba4
-cp -rf $FT_SAMBA_DIR/samba-4.9.16/* $FT_MIPS_REPODIR/release/src/router/samba4
-cp -rf $FT_SAMBA_DIR/answer_positiv.txt $FT_MIPS_REPODIR/release/src/router/samba4/answer.txt
-cp -rf $FT_SAMBA_DIR/Makefile_samba4 $FT_MIPS_REPODIR/release/src/router/samba4/Makefile
-mkdir $FT_MIPS_REPODIR/release/src/router/libtirpc
-cp -rf $LIBTIRPC_DIR/* $FT_MIPS_REPODIR/release/src/router/libtirpc
+mkdir $FT_REPO_DIR/release/src/router/samba4
+cp -rf $FT_SAMBA_DIR/samba-4.9.16/* $FT_REPO_DIR/release/src/router/samba4
+cp -rf $FT_SAMBA_DIR/answer_positiv.txt $FT_REPO_DIR/release/src/router/samba4/answer.txt
+cp -rf $FT_SAMBA_DIR/Makefile_samba4 $FT_REPO_DIR/release/src/router/samba4/Makefile
+patch -i $FT_SAMBA_DIR/Makefile_samba_libtirpc.patch $FT_REPO_DIR/release/src/router/Makefile
+mkdir $FT_REPO_DIR/release/src/router/libtirpc
+cp -rf $LIBTIRPC_DIR/* $FT_REPO_DIR/release/src/router/libtirpc
 
-#
-## Arch-Linuc patch for RT-AC-images:
-#patch -i $FT_MIPS_ARTIX_PATCHDIR/mksquashfs.c.patch $FT_MIPS_REPODIR/release/src-rt-6.x/linux/linux-2.6/scripts/squashfs/mksquashfs.c
-#cd release/src-rt-6.x
-#make clean
+## for RT-AC-images:
+cd release/src-rt-6.x
 make wndr4500v2z ## > build.txt; AIO: z; VPN: e
 
 ## only RT-N-Images
-cd release/src-rt
-patch -i $HOME/Dokumente/freshtomato-mips/mksquashfs.c.patch $HOME/freshtomato-mips/release/src-rt/linux/linux-2.6/scripts/squashfs/mksquashfs.c 
-make n64z ## > build.txt; AIO: z; VPN: e; 
+#cd release/src-rt
+#make n64z ## > build.txt; AIO: z; VPN: e; 
 
 
